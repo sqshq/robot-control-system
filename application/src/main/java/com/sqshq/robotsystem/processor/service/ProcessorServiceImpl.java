8228e3e2eff533cd7403fbdb5a14825762905800
sqshq <sqshq@sqshq.com>
2017-05-15 23:55:19 +0300

    fix formatting

--- application/src/main/java/com/sqshq/robotsystem/processor/service/ProcessorServiceImpl.java
+++ application/src/main/java/com/sqshq/robotsystem/processor/service/ProcessorServiceImpl.java
@@ -15,16 +15,16 @@ public class ProcessorServiceImpl implements ProcessorService {
 
     private int nthPrime(int n) {
         int candidate, count;
-        for(candidate = 2, count = 0; count < n; ++candidate) {
+        for (candidate = 2, count = 0; count < n; ++candidate) {
             if (isPrime(candidate)) {
                 ++count;
             }
         }
-        return candidate-1;
+        return candidate - 1;
     }
 
     private boolean isPrime(int n) {
-        for(int i = 2; i < n; ++i) {
+        for (int i = 2; i < n; ++i) {
             if (n % i == 0) {
                 return false;
             }
